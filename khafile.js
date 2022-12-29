b3c01e3d61b4b530a2b8ccbcfd7b03dd398cb5ad
zzox <tweedlitweedli@gmail.com>
2022-12-29 12:20:01 -0500

    remove astar lib, add preload image

--- khafile.js
+++ khafile.js
@@ -3,7 +3,6 @@ const project = new Project('boilerplate');
 project.addAssets('assets/**');
 project.addShaders('shaders/**');
 project.addSources('src');
-project.addLibrary('astar');
 // project.addDefine('debug_physics');
 
 resolve(project);
