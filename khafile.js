fdab7f0a99b6aa439efd3fb647af92317a131dfb
Tyler McCarthy <tweedlitweedli@gmail.com>
2023-04-10 21:07:58 -0500

    new autosizing for builds

--- khafile.js
+++ khafile.js
@@ -1,8 +1,22 @@
-const project = new Project('boilerplate');
+const project = new Project('quirk-boilerplate');
 
-project.addAssets('assets/**');
+// alternative to project.addAssets('assets/**');
+project.addAssets('assets/images/**/*.png');
+project.addAssets('assets/data/**/*.json');
+project.addAssets('assets/data/**/*.ldtk');
+project.addAssets('assets/data/**/*.tmx');
+project.addAssets('assets/music/**/*.wav');
+project.addAssets('assets/audio/**/*.wav');
+project.addAssets('assets/fonts/**/*.ttf');
+project.addAssets('assets/fonts/**/*.png');
+project.addAssets('assets/index.html');
+
 project.addShaders('shaders/**');
 project.addSources('src');
 // project.addDefine('debug_physics');
+// project.addDefine('is_ng');
+// project.addLibrary('newgrounds');
+
+project.addDefine('kha_html5_disable_automatic_size_adjust');
 
 resolve(project);
--- src/Main.hx
+++ src/Main.hx
@@ -2,8 +2,48 @@ import core.Game;
 import core.Types;
 import game.scenes.TestScene;
 
+#if kha_html5
+import js.Browser.document;
+import js.Browser.window;
+import js.html.CanvasElement;
+import kha.Macros;
+#end
+
 class Main {
-	public static function main() {
+    public static function main() {
+        setFullWindowCanvas();
         new Game(new IntVec2(1300, 750), new TestScene(), PixelPerfect, 'boilerplate', new IntVec2(320, 180));
-	}
+    }
+
+    // This handles the resizing on our own so we don't rely on kha's.
+    // Requires `kha_html5_disable_automatic_size_adjust` define.
+    static function setFullWindowCanvas() {
+        #if kha_html5
+        document.documentElement.style.padding = '0';
+        document.documentElement.style.margin = '0';
+        document.body.style.padding = '0';
+        document.body.style.margin = '0';
+        final canvas:CanvasElement = cast document.getElementById(Macros.canvasId());
+        canvas.style.display = 'block';
+        canvas.style.imageRendering = 'pixelated';
+        final resize = function() {
+            var w = document.documentElement.clientWidth;
+            var h = document.documentElement.clientHeight;
+            if (w == 0 || h == 0) {
+                w = window.innerWidth;
+                h = window.innerHeight;
+            }
+            // canvas.width = Std.int(w * window.devicePixelRatio);
+            // canvas.height = Std.int(h * window.devicePixelRatio);
+            canvas.width = Std.int(w);
+            canvas.height = Std.int(h);
+            // if (canvas.style.width == '') {
+            // 	canvas.style.width = '100%';
+            // 	canvas.style.height = '100%';
+            // }
+        }
+        window.onresize = resize;
+        resize();
+        #end
+    }
 }
